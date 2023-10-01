1fb21d68f47bffd93ac751c4e22e5e3589e20bff
MuhammadJawaar <muhammadjawaar18@gmail.com>
2023-10-01 15:14:16 +0700

    image uploader

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ app.use(bodyParser.json());
 const accountRoute = require('./routes/account');
 const adminRoute = require('./routes/admin');
 const userRoute = require('./routes/user');
+const imageRoute = require('./routes/image');
 
 app.use("/account",accountRoute);
 app.use("/admin",adminRoute);
 app.use("/user",userRoute);
+app.use("/images",imageRoute);
+app.use("/uploads", express.static('uploads'));
 
 module.exports = app;
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const adminController = require('../controllers/admin.controller');
 const checkAuthMiddleware = require('../middleware/check-auth');
+const imageUploader = require('../helpers/image-uploader')
 
 const router = express.Router();
 
@@ -18,7 +19,16 @@ router.get("/presensi/negatif", checkAuthMiddleware.checkAuthAdmin, adminControl
 router.get("/presensi/:id", checkAuthMiddleware.checkAuthAdmin, adminController.showPresensiPerPeserta)
 
 router.get("/tugas", checkAuthMiddleware.checkAuthAdmin, adminController.showTugasAll);
-router.post("/tugas/add", checkAuthMiddleware.checkAuthAdmin, adminController.addTugas);
+router.post("/tugas/add", checkAuthMiddleware.checkAuthAdmin,imageUploader.upload.single('image'), (req, res) =>{
+    if (!req.file) {
+        return res.status(400).json({
+          message: 'No file uploaded',
+        });
+      }
+
+      const uploadedFileUrl = req.file.path;
+      adminController.addTugas(req,res,uploadedFileUrl);
+});
 router.get("/tugas/:id", checkAuthMiddleware.checkAuthAdmin, adminController.showTugasStatusByTugas);
 router.delete("/tugas/:id/delete", checkAuthMiddleware.checkAuthAdmin, adminController.deleteTugas);
 
