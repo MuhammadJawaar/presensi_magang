14716e9f3abf295b0ec00d87dc7b954db575b96f
mbprayoga <entdecker000@gmail.com>
2023-10-18 14:40:15 +0700

    server

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log('http:/localhost:3000'));
+server.listen(port, () => console.log('http://localhost:3000'));
