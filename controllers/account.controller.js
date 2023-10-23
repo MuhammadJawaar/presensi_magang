d3465a2fa7e794fb30fe4503da71f8cf8b0440e6
MuhammadJawaar <muhammadjawaar18@gmail.com>
2023-10-23 18:51:00 +0700

    refresh token

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -105,7 +105,50 @@ async function logout(req, res) {
     }
 }
 
+const refreshToken = async(req,res) => {
+    try {
+        const refreshtoken = req.cookies.refreshtoken;
+        if (!refreshtoken){
+            return res.status(401).json({
+                'msg': "cookies bertingkah aneh"
+            });
+        }
+        const role = req.userData.role;
+        if (role === 'peserta_magang' || role === 'admin') {
+            const userModel = role === 'peserta_magang' ? models.Peserta_Magang : models.Admin;
+            const user = await userModel.findOne({ where: { refreshTokens: refreshtoken } });
+
+            if (!user){
+                return res.status(403).json({
+                    'mesg': "ini lu bukan ?"
+                });
+            }
+            jwt.verify(refreshtoken, process.env.REFRESH_TOKEN_SECRET, (err,decoded) =>{
+                if(err){
+                    return res.status(403).json({
+                        'message': "somethin went wrong "
+                    });
+                }
+                const token = jwt.sign({
+                    username: user.username,
+                    userId: user.id,
+                    role: role
+                }, process.env.JWT_KEY, {
+                    expiresIn: '15m' // Set the token expiration time (e.g., 15 minutes)
+                });
+                console.log(token);
+            });
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            "message": "something wen wrong"
+        })
+    }
+}
+
 module.exports = {
     login: login,
-    logout:logout
+    logout:logout,
+    refreshToken:refreshToken
 };
