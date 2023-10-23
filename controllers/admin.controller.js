d1cb3ba2dba34ce426e7aa6ae4c0804b4232b384
MuhammadJawaar <muhammadjawaar18@gmail.com>
2023-10-23 09:54:59 +0700

    redo

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -681,7 +681,6 @@ async function exportAdmin(req, res) {
 async function exportPeserta(req, res) {
     try {
       const results = await models.Peserta_Magang.findAll();
-      console.log(results);
   
       const workbook = new exceljs.Workbook();
       const sheet = workbook.addWorksheet('Peserta Magangs');
